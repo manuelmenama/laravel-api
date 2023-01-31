3706d0afe45d35f7f87826d3fa16eb556149e0ef
manuelmenama <manuel.mena090589@gmail.com>
2023-01-31 21:40:29 +0100

    added validation

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,14 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request){
         $data = $request->all();
 
-        //verificare i dati
+        $success = true;
 
+        //verificare i dati
+        $validator = Validator::make($data,
+            [
+
+                "object" => "required|min:3|max:255",
+                "email" => "required|email|max:255",
+                "subject" => "required|min:5",
+
+            ],
+            [
+                //secondo oggetto per i dati personalizzati
+            ]
+        );
+        if($validator->fails()){
+
+            $errors = $validator->errors();
+            $success = false;
+
+            return response()->json(compact('errors', 'success'));
+
+        }
         //salvare nel db
 
         //inviare email
