3c84556e0a80eb0383e91a3e893030e339bcdb3f
manuelmenama <manuel.mena090589@gmail.com>
2023-02-09 12:54:08 +0100

    none

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     <div class="container mt-5">
-        <h3>Sono presenti: {{ $project_counter }} progetti</h3>
+        <h3>Sono presenti: {{ $project_counter }} progetti di {{Auth::user()->name}}</h3>
     </div>
 
 @endsection
